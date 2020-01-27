bf80cd75add6a0ae2c670b3d833858d65a7240f7
Vassili LAROQUE <vlaroque@student.42.fr>
2020-01-27 21:21:02 +0100

    non working multichamps

--- obj/vm/champ_load.d
+++
@@ -1,2 +0,0 @@
-obj/vm/champ_load.o: sources/vm/champ_load.c libft/libft.h includes/op.h \
-  includes/corewar.h
--- obj/vm/main.d
+++
@@ -1,2 +0,0 @@
-obj/vm/main.o: sources/vm/main.c libft/libft.h includes/op.h \
-  includes/corewar.h
